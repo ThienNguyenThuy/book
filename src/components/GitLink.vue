238775f203beed0bcda5f3ea3223cb620355ee03
Fressor <88276438+Fressor@users.noreply.github.com>
2022-01-09 21:45:00 +0000

    Update GitLink.vue

--- src/components/GitLink.vue
+++ src/components/GitLink.vue
@@ -1,5 +1,5 @@
 <template>
-  <a href="https://github.com/samuelhorn/jamdocs" :class="size" title="Git-repository" aria-label="Jamdocs on Github">
+  <a href="https://github.com/CloneJS" :class="size" title="Git-repository" aria-label="AlexJS on Github">
     <github-icon class="icon" />
     {{text}}
   </a>
